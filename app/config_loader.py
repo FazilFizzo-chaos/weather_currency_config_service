41c295d896617f7f916bc1f835a902bbbb266111
FazilChaos <fadhilimajala97@gmail.com>
2025-12-16 00:05:11 +0300

    Add loading default config for the app

--- app/config_loader.py
+++ app/config_loader.py
@@ -9,11 +9,19 @@ env = os.getenv("APP_ENV", "dev")
 BASE_DIR = Path(__file__).resolve().parent # base dir of this file
 PROJECT_ROOT = BASE_DIR.parent # root dir of the whole project
 CONFIG_DIR = PROJECT_ROOT / "config" # config files dir
-config_file_path = CONFIG_DIR / f"{env}.json"
+config_file_env_path = CONFIG_DIR / f"{env}.json"
 
 def load_json():
     try:
-      with open(config_file_path, encoding="utf-8") as config_file:
+      with open(config_file_env_path, encoding="utf-8") as config_file:
         return json.load(config_file)
     except FileNotFoundError:
        raise FileNotFoundError(f"Config file not found: config/{env}.json")
+
+config_default_path = CONFIG_DIR / "defaults.json"
+def load_default_config():
+    try:
+      with open(config_default_path, encoding="utf-8") as f:
+        return json.load(f)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Config file not found: {config_default_path}")
