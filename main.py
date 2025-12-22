e30d44aa720f89531dacf95ee4877708012e1e63
FazilChaos <fadhilimajala97@gmail.com>
2025-12-22 18:19:05 +0300

    Integrate merging and override configs into main.py

--- main.py
+++ main.py
@@ -6,42 +6,57 @@ import json
 
 def main():
  args = parse_args()
- load_env(args.env)
- default_config = load_default_config()
 
+ load_env(args.env)
  api_currency_key = os.getenv("API_KEY_CURRENCY")
  api_weather_key = os.getenv("API_KEY_WEATHER")
 
- currency_base_url = default_config["api"]["currency_url"]
  currency = args.currency
- print(fetch_conversion_rate_currency(currency_base_url, api_currency_key, currency))
-
  first_currency = args.first_currency
  second_currency = args.second_currency
  amount = args.amount
- if amount:
-  print(fetch_conversion_rate_btn_two_currencies(currency_base_url, api_currency_key, first_currency, second_currency, amount))
- else:
-  print(fetch_conversion_rate_btn_two_currencies(currency_base_url, api_currency_key, first_currency, second_currency))
-
- geo_location_url = default_config["api"]["geocode_url"]
  city_name = args.city_name
  state_code = args.state_code
  country_code = args.country_code
 
- geo_data = fetch_geo_location(geo_location_url, api_weather_key, city_name, state_code, country_code)[0]
- geo_lat = geo_data["lat"]
- geo_lon = geo_data["lon"]
-
- weather_url = default_config["api"]["weather_url"]
- weather_data = fetch_current_weather_data(weather_url, api_weather_key, geo_lat, geo_lon)
- print(weather_data)
 
  override_env_config = load_json(args.env)
  default_config = load_default_config()
 
- merge_config = deep_merge_conflict(default_config, override_env_config)
- print(merge_config) # this is a merged config, dev config overrides default and merges with default
+ if args.env is None:
+  default_config = load_default_config()
+  currency_base_url = default_config["api"]["currency_url"]
+  print(fetch_conversion_rate_currency(currency_base_url, api_currency_key, currency))
+  if amount:
+   print(fetch_conversion_rate_btn_two_currencies(currency_base_url, api_currency_key, first_currency, second_currency,
+                                                  amount))
+  else:
+   print(fetch_conversion_rate_btn_two_currencies(currency_base_url, api_currency_key, first_currency, second_currency))
+
+  geo_location_url = default_config["api"]["geocode_url"]
+  geo_data = fetch_geo_location(geo_location_url, api_weather_key, city_name, state_code, country_code)[0]
+  geo_lat = geo_data["lat"]
+  geo_lon = geo_data["lon"]
+
+  weather_url = default_config["api"]["weather_url"]
+  weather_data = fetch_current_weather_data(weather_url, api_weather_key, geo_lat, geo_lon)
+ elif args.env:
+  merge_config = deep_merge_conflict(default_config, override_env_config)
+  currency_merge_config_url = merge_config["api"]["currency_url"]
+  print(fetch_conversion_rate_currency(currency_merge_config_url, api_currency_key, currency))
+  if amount:
+   print(fetch_conversion_rate_btn_two_currencies(currency_merge_config_url, api_currency_key, first_currency, second_currency,
+                                                  amount))
+  else:
+   print(fetch_conversion_rate_btn_two_currencies(currency_merge_config_url, api_currency_key, first_currency, second_currency))
+
+  geo_location_url = merge_config["api"]["geocode_url"]
+  geo_data = fetch_geo_location(geo_location_url, api_weather_key, city_name, state_code, country_code)[0]
+  geo_lat = geo_data["lat"]
+  geo_lon = geo_data["lon"]
+
+  weather_url = merge_config["api"]["weather_url"]
+  weather_data = fetch_current_weather_data(weather_url, api_weather_key, geo_lat, geo_lon)
 
 
 if __name__ == "__main__":
